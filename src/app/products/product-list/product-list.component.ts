e80a1a754eb99ad77d3692ef1c35b75a80f46768
firatadar <firatadar123@gmail.com>
2025-01-02 22:49:31 +0300

    bir şeyler yapıldı

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -8,32 +8,41 @@ import { Product } from '../product.model';
   selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  providers: [ProductService]
+  providers: [ProductService],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
+  filteredProducts: Product[] = [];
   loading: boolean = false;
+  searchTerm: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private cartService: CartService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.loading = true;
 
-      this.productService.getProducts(params["categoryId"]).subscribe(data => {
-        this.products = data;
-        this.loading = false;
-      });
-
+      this.productService
+        .getProducts(params['categoryId'])
+        .subscribe((data) => {
+          this.products = data;
+          this.filteredProducts = data;
+          this.loading = false;
+        });
     });
   }
 
   addToCart(product: Product): void {
     this.cartService.addItem(product);
+  }
+
+  filterProducts(): void {
+    this.filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
